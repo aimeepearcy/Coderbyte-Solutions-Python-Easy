b419d1b493a6a89e0dc4bfd09c97346c80fde2b8
Aimee Pearcy <aimee.pearcy@gmail.com>
2018-11-08 11:52:21 +0000

    Resolved 2 issues
    - Code can now detect if a letter is a vowel and capitalize it if it is
    - Code now ignores special characters and numbers and adds them back to the new string as normal 
    The program now works as it should

--- 04. LetterChanges.py
+++ 04. LetterChanges.py
@@ -1,3 +1,7 @@
+# Have the function LetterChanges(str) take the str parameter being passed and modify it using the following algorithm. 
+# Replace every letter in the string with the letter following it in the alphabet (ie. c becomes d, z becomes a). 
+# Then capitalize every vowel in this new string (a, e, i, o, u) and finally return this modified string. 
+
 from string import ascii_letters
 from string import ascii_uppercase
 
@@ -8,36 +12,23 @@ def LetterChanges(str):
     
     # Find the location of each letter from the original string in the ascii string
     for letter in str:
-        ascii_position = ascii_letters.find(letter)
-        # Add the ascii letter at this location to the new string 
-        new_string += ascii_letters[ascii_position + 1]
-    print(new_string)
-    
-    # Capitalize the vowels
-    for letter in new_string:
-        if letter == 'a' or 'e' or 'i' or 'o' or 'u':
-            new_string = new_string.replace(letter, letter.upper())
+        # Check to see if the letter is in the alphabet 
+        if letter in (ascii_letters or ascii_uppercase):
+            # Find its position in the ascii_letters string 
+            ascii_position = ascii_letters.find(letter)
+            # Move the position up by one and add the ascii letter at this location to the new string 
+            new_string += ascii_letters[ascii_position + 1]
+        # If the letter is not in the alphabet (e.g. a special character or a number), add it to the new string as normal
         else:
-            pass
-    print(new_string)
+            new_string += letter
+    
+    for letter in new_string:
+        # Check to see if the letter is a vowel
+        if letter in ('a', 'e', 'i', 'o', 'u'):
+            # If it is a lowercase vowel, replace it with a capitalized version
+            new_string = new_string.replace(letter, letter.upper())
+    return new_string
 
 
 # keep this function call here  
-print(LetterChanges("cat!!"))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
+print(LetterChanges(raw_input()))
